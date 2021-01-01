975f0e96f58a67ba68608c11815c6e2b02e971d9
DFagan21 <47934967+DFagan21@users.noreply.github.com>
2021-01-01 12:34:32 +1100

    Before adding authenticated routes Notes up and running but doens't have authentication denys for certain pages based on session state

--- src/config.js
+++ src/config.js
@@ -11,8 +11,9 @@ const config = {
     REGION: "ap-southeast-2",
     USER_POOL_ID: "ap-southeast-2_d3ATAJIQA",
     APP_CLIENT_ID: "6nqan0b1s9ctm1ohnf36l0ki1s",
-    IDENTITY_POOL_ID: "0565a377-77e8-4d7b-a184-8a6539c20104"
-  }
+    IDENTITY_POOL_ID: "ap-southeast-2:0565a377-77e8-4d7b-a184-8a6539c20104"
+  },
+  MAX_ATTACHMENT_SIZE: 5000000
 };
 
 export default config;
