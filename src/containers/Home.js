3534f4f6a6efe0c1ae729cbd58feed2da46fe1e5
DFagan21 <47934967+DFagan21@users.noreply.github.com>
2021-01-12 00:25:36 +1100

    FastCAM pre cognito swap With updated tabs for fake links

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,8 +6,8 @@ import "./Home.css";
 import { API } from "aws-amplify";
 import { BsPencilSquare } from "react-icons/bs";
 import { LinkContainer } from "react-router-bootstrap";
-import Tabs from 'react-bootstrap/Tabs'
-import Tab from 'react-bootstrap/Tab'
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
@@ -50,9 +50,7 @@ export default function Home() {
           <LinkContainer key={submissionid} to={`/users/${submissionid}`}>
             <ListGroup.Item action>
               <span className="font-weight-bold">
-                {firstname}
-                {" "}
-                {lastname}
+                {firstname} {lastname}
               </span>
               <br />
               <span className="text-muted">
@@ -68,86 +66,99 @@ export default function Home() {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <h1>Reseller Portal</h1>
+        <p className="text-muted">Submission of user portal</p>
       </div>
     );
   }
 
-function infoTab() {
-  return(
-    <div>
-      <p>
-        <br/>
-        Welcome to the FastCAM Reseller page!
-        FastCAM works with many OEMs & Resellers of machines to ensure that your customers get the best FastCAM Software, right for their needs. To this end, we do offer sizeable discounts, based on volume, for FastCAM resellers. 
-        <br/>
-        <br/>
-      </p>
-      <ul>
-    <div>
-        <h1>
-            The Discount is Store wide!
-        </h1>
-        <li>
-            This discount applies across the whole FastCAM Store! So you can buy any product discounted from the FastCAM online catalogue on behalf of your customer! 
-        </li>
-    </div>
-    <div>
-    <h1>
-    <br/>
-        FastCAM 8 Annual Discounts & remuneration
-    </h1>
-        <li>
-                Every Year you will be entitled to the discount and can sell to your customers.
-        </li>
-        <li>
-                This enables you to sell to your customer at a discount, and retain a portion of the sale as well.
-        </li>
-        <li>
-                This also applies for future renewals on our FastCAM 8 Subscriptions! This means with FastCAM you can generate perpetual income!
-        </li>
-    </div>
-    <div>
-        <h1>
-        <br/>
-        FastCAM 8 Training
-        </h1>
-        <li>
-            As with all customer of FastCAM 8, your customers will also be eligible for access to our Online Training system. You just need to fill out their details after purchase, and we can grant them access to the system
-        </li>
-    </div>
-    <div>
-        <h1>
-        <br/>
-        FastCAM 8 Support
-        </h1>
-        <li>
-            A Part of the deal is that you, as the Point of Contact, will also be the first line of support for FastCAM as well. So technical questions will need to be answered by you. However, if you have any over the top questions, or ones you cannot answer, we are happy to help you help your customer. 
-        </li>
-    </div>
-    <div>
-        <h1>
-        <br/>
-        FastCAM is in ACTIVE development!
-        </h1>
-        <li>
-            As the product is being worked on daily, we can incorporate changes you may find useful for your customers into the software! However, please be aware these are subject to review and approval
-        </li>
-    </div>
-</ul>
-<p>
-    <br/>
-    <br/>
-    To qualify for this process, please fill out signup form and we will be in touch shortly!
-</p>
+  function infoTab() {
+    return (
+      <div>
+        <p>
+          <br />
+          Welcome to the FastCAM Reseller page! FastCAM works with many OEMs &
+          Resellers of machines to ensure that your customers get the best
+          FastCAM Software, right for their needs. To this end, we do offer
+          sizeable discounts, based on volume, for FastCAM resellers.
+          <br />
+          <br />
+        </p>
+        <ul>
+          <div>
+            <h1>The Discount is Store wide!</h1>
+            <li>
+              This discount applies across the whole FastCAM Store! So you can
+              buy any product discounted from the FastCAM online catalogue on
+              behalf of your customer!
+            </li>
+          </div>
+          <div>
+            <h1>
+              <br />
+              FastCAM 8 Annual Discounts & remuneration
+            </h1>
+            <li>
+              Every Year you will be entitled to the discount and can sell to
+              your customers.
+            </li>
+            <li>
+              This enables you to sell to your customer at a discount, and
+              retain a portion of the sale as well.
+            </li>
+            <li>
+              This also applies for future renewals on our FastCAM 8
+              Subscriptions! This means with FastCAM you can generate perpetual
+              income!
+            </li>
+          </div>
+          <div>
+            <h1>
+              <br />
+              FastCAM 8 Training
+            </h1>
+            <li>
+              As with all customer of FastCAM 8, your customers will also be
+              eligible for access to our Online Training system. You just need
+              to fill out their details after purchase, and we can grant them
+              access to the system
+            </li>
+          </div>
+          <div>
+            <h1>
+              <br />
+              FastCAM 8 Support
+            </h1>
+            <li>
+              A Part of the deal is that you, as the Point of Contact, will also
+              be the first line of support for FastCAM as well. So technical
+              questions will need to be answered by you. However, if you have
+              any over the top questions, or ones you cannot answer, we are
+              happy to help you help your customer.
+            </li>
+          </div>
+          <div>
+            <h1>
+              <br />
+              FastCAM is in ACTIVE development!
+            </h1>
+            <li>
+              As the product is being worked on daily, we can incorporate
+              changes you may find useful for your customers into the software!
+              However, please be aware these are subject to review and approval
+            </li>
+          </div>
+        </ul>
+        <p>
+          <br />
+          <br />
+          To qualify for this process, please fill out signup form and we will
+          be in touch shortly!
+        </p>
+      </div>
+    );
+  }
 
-    </div>
-  )
-  
-}
-
-  
   function renderSubmissions() {
     return (
       <div className="notes">
@@ -156,20 +167,30 @@ function infoTab() {
       </div>
     );
   }
+  function customerlinks() {
+    return (
+      <div>
+        <span>You can see the OEM Links</span>
+      </div>
+    );
+  }
+  function filler() {
+    return <div>Please login</div>;
+  }
 
   return (
     <div className="Home">
       <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
-  <Tab eventKey="Submissions" title="Submissions">
-    {isAuthenticated ? renderSubmissions() : renderLander()}
-  </Tab>
-  <Tab eventKey="Information" title="Information">
-  {infoTab()}
-  </Tab>
-  <Tab eventKey="MoreInfo" title="More Info">
-  <span> 3</span>
-  </Tab>
-</Tabs>
+        <Tab eventKey="Submissions" title="Submissions">
+          {isAuthenticated ? renderSubmissions() : renderLander()}
+        </Tab>
+        <Tab eventKey="Information" title="Information">
+          {infoTab()}
+        </Tab>
+        <Tab eventKey="" title="Oem Links">
+          {isAuthenticated ? customerlinks() : filler()}
+        </Tab>
+      </Tabs>
     </div>
   );
 }
